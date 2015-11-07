ebddc4fda6957770f167c132662a44563a3b6ca2
Craig Simpson <c.simps@gmail.com>
2015-11-07 23:24:02 +0000

    Fixing alignment.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,8 @@
 // Include Gulp.
 var gulp        = require( 'gulp' ),
-    requireDir = require('require-dir');
+    requireDir 	= require('require-dir');
 
-var load = requireDir('./tasks', { recurse: true } );
+var load 		= requireDir('./tasks', { recurse: true } );
 
 // Default Tasks.
 gulp.task( 'default' , [ 'watch' ] );
