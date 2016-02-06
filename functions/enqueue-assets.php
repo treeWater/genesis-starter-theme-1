4d52c6f4c61ce0437d547eb3e5ddad08959f2539
Craig Simpson <craig@Craigs-MBP.lan>
2016-02-06 12:13:09 +0000

    Slight changes to structure, moving built files to /assets/ dir.

--- functions/enqueue-assets.php
+++ functions/enqueue-assets.php
@@ -30,7 +30,7 @@ add_action( 'wp_enqueue_scripts', 'gst_load_scripts' );
 function gst_load_scripts() {
 
 	wp_dequeue_script( 'comment-reply' );
-	wp_enqueue_script( 'gst-theme', get_stylesheet_directory_uri() . '/js/theme.js', [ 'jquery' ], '1.0.0', true );
+	wp_enqueue_script( 'gst-theme', get_stylesheet_directory_uri() . '/assets/js/theme.js', [ 'jquery' ], '1.0.0', true );
 
 }
 
@@ -42,7 +42,7 @@ add_filter( 'genesis_pre_load_favicon', 'gst_pre_load_favicon' );
  */
 function gst_pre_load_favicon() {
 
-	return get_stylesheet_directory_uri() . '/images/favicon.ico';
+	return get_stylesheet_directory_uri() . '/assets/images/favicon.ico';
 
 }
 
