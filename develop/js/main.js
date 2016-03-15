fdfb2871652097c3c516bb38e7288457523492af
Craig Simpson <c.simps@gmail.com>
2016-03-15 10:31:06 +0000

    Switching to module pattern for custom theme JS, props to @garyjones. // https://gamajo.com/javascript-module-pattern-example

--- develop/js/main.js
+++ develop/js/main.js
@@ -1,77 +1,33 @@
-/* ========================================================================
- * DOM-based Routing
- * Based on http://goo.gl/EUTi53 by Paul Irish
- *
- * Only fires on body classes that match. If a body class contains a dash,
- * replace the dash with an underscore when adding it to the object below.
- *
- * .noConflict()
- * The routing is enclosed within an anonymous function so that you can
- * always reference jQuery with $, even when in .noConflict() mode.
- * ======================================================================== */
+var genesis_starter_theme = (function( $ ) {
+	'use strict';
 
-(function($) {
+	/**
+	 * Empty placholder function.
+	 *
+	 * @since 1.0.0
+	 */
+	var functionName = function() {
+		// Empty function, do all the things.
+	},
 
-  // Use this variable to set up the common and page specific functions. If you
-  // rename this variable, you will also need to rename the namespace below.
-  var Genesis_Starter_Theme = {
-    // All pages
-    'common': {
-      init: function() {
-        // JavaScript to be fired on all pages
-      },
-      finalize: function() {
-        // JavaScript to be fired on all pages, after page specific JS is fired
-      }
-    },
-    // Home page
-    'home': {
-      init: function() {
-        // JavaScript to be fired on the home page
-      },
-      finalize: function() {
-        // JavaScript to be fired on the home page, after the init JS
-      }
-    },
-    // About us page, note the change from about-us to about_us.
-    'about_us': {
-      init: function() {
-        // JavaScript to be fired on the about us page
-      }
-    }
-  };
+	/**
+	 * Fire events on document ready, and bind other events.
+	 *
+	 * @since 1.0.0
+	 */
+	ready = function() {
+		functionName();
 
-  // The routing fires all common scripts, followed by the page specific scripts.
-  // Add additional events for more control over timing e.g. a finalize event
-  var UTIL = {
-    fire: function(func, funcname, args) {
-      var fire;
-      var namespace = Genesis_Starter_Theme;
-      funcname = (funcname === undefined) ? 'init' : funcname;
-      fire = func !== '';
-      fire = fire && namespace[func];
-      fire = fire && typeof namespace[func][funcname] === 'function';
+    // Examples binding to events.
+		$( window ).on( 'resize.genesis_starter_theme', functionName );
+		$( window ).on( 'scroll.genesis_starter_theme resize.genesis_starter_theme', functionName );
+	};
 
-      if (fire) {
-        namespace[func][funcname](args);
-      }
-    },
-    loadEvents: function() {
-      // Fire common init JS
-      UTIL.fire('common');
+	// Only expose the ready function to the world
+	return {
+		ready: ready
+	};
 
-      // Fire page-specific init JS, and then finalize JS
-      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
-        UTIL.fire(classnm);
-        UTIL.fire(classnm, 'finalize');
-      });
+})( jQuery );
 
-      // Fire common finalize JS
-      UTIL.fire('common', 'finalize');
-    }
-  };
-
-  // Load Events
-  $(document).ready(UTIL.loadEvents);
-
-})(jQuery); // Fully reference jQuery after this point.
+jQuery( genesis_starter_theme.ready );
