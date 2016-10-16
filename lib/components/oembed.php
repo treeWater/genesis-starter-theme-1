9aeb9fb75a2452eb5877d3a4ee31a3f0e8f25527
Craig Simpson <craig@craigsimpson.scot>
2016-10-16 21:34:48 +0100

    Fix file and function comments, and minor code smell.

--- lib/components/oembed.php
+++ lib/components/oembed.php
@@ -2,22 +2,24 @@
 /**
  * Wrap oEmbed content.
  *
- * @package   Project\Theme
- * @author    Craig Simpson
- * @license   GPL-2.0+
- * @copyright 2016 Gamajo Tech
+ * @author  Craig Simpson
+ * @package Project\Theme
+ * @since   1.0.0
  */
 
 namespace Project\Theme;
 
-add_filter('embed_oembed_html', __NAMESPACE__ . '\embed_wrap');
+add_filter( 'embed_oembed_html', __NAMESPACE__ . '\embed_wrap' );
 /**
  * Wrap embedded media as suggested by Readability
  *
  * @link https://gist.github.com/965956
- * @link http://www.readability.com/publishers/guidelines#publisher
+ * @link https://gist.github.com/965956@link http://www.readability.com/publishers/guidelines#publisher
  *
  * @since 1.0.0
+ *
+ * @param $cache
+ * @return string
  */
 function embed_wrap( $cache ) {
 	return '<div class="entry-content-asset">' . $cache . '</div>';
