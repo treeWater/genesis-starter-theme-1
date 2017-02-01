6af6cff275c603cf560c478d3411d398b0e33a58
Gary Jones <gary@gamajo.com>
2017-02-01 09:09:18 +0000

    Improve oEmbed wrapping

--- src/components/oembed.php
+++ src/components/oembed.php
@@ -7,20 +7,26 @@
  * @since   1.0.0
  */
 
+declare ( strict_types = 1 );
+
 namespace Project\Theme;
 
-add_filter( 'embed_oembed_html', __NAMESPACE__ . '\embed_wrap' );
+add_filter( 'oembed_dataparse', __NAMESPACE__ . '\embed_wrap', 10, 3 );
 /**
- * Wrap embedded media as suggested by Readability
- *
- * @link https://gist.github.com/965956
- * @link http://www.readability.com/publishers/guidelines#publisher
+ * Wrap oEmbed generated HTML with divs that allow for styling them.
  *
  * @since 1.0.0
  *
- * @param $cache
- * @return string
+ * @link https://plugins.svn.wordpress.org/oembed-styling/tags/1.1/oEmbed-styling.php
+ *
+ * @param string     $html oEmbed HTML.
+ * @param \WP_oEmbed $data oEmbed data.
+ * @param string     $url  oEmbed URL.
+ * @return string Wrapped oEmbed HTML.
  */
-function embed_wrap( $cache ) {
-	return '<div class="entry-content-asset">' . $cache . '</div>';
+function embed_wrap( string $html, \WP_oEmbed $data, string $url ) {
+	preg_match( '#(http|ftp)s?://(www\.)?([a-z0-9\.\-]+)/?.*#i', $url, $matches );
+	$domain = str_replace( '.', '-', $matches[3] );
+
+	return "<div class=\"oembed oembed-$data->type oembed-$domain oembed-$data->type-$domain\">$html</div>";
 }
