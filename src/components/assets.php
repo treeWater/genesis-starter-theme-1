444d9316d029ca7fa181c6538736e4c120484b87
Gary Jones <gary@gamajo.com>
2017-02-02 08:21:05 +0000

    Turn on / configure stricter PHPCS checks
    Fix the one instance where the code wasn't following these stricter checks already.
    
    Some of these checks don't exist until WPCS 0.11, but work fine if running the `develop` branch of it.

--- src/components/assets.php
+++ src/components/assets.php
@@ -34,7 +34,5 @@ add_filter( 'genesis_pre_load_favicon', __NAMESPACE__ . '\pre_load_favicon' );
  * @since 1.0
  */
 function pre_load_favicon() {
-
 	return CHILD_THEME_URI . '/assets/images/favicon.ico';
-
 }
